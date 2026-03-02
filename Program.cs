0f657b9b116c991b70d414e9cc8829777153fe6c
nikita139584 <staskonikita139@gmail.com>
2026-03-02 18:49:21 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp1.Infrastructure;
+using ConsoleApp1.Infrastructure.Repositories;
+using ConsoleApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            var context = new AppDbContext();
+            context.Database.Migrate(); // dotnet ef database update
+
+            var repo = new UserRepository(context);
+
+            repo.AddUsers(new List<User>
+            {
+                new User { Name = "Alice", Age = 44 },
+                new User { Name = "Bob", Age = 44 },
+                new User { Name = "Charlie", Age = 44 }
+            });
+
+            repo.ShowAllUsers(); // показуємо усіх користувачів
+        }
+    }
+}
